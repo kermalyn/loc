42e653f6dd65f7a47250b958a2b66cf9815bf7ba
CSC2020 <33274745+CSC2020@users.noreply.github.com>
2019-10-15 20:08:30 -0400

    updated plain2

--- a2plain.c
+++ a2plain.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-
 #include <a2plain.h>
 #include "uarray2.h"
 
@@ -7,61 +6,59 @@
 typedef A2Methods_Array2 A2A;
 
 static A2Methods_Array2 new(int width, int height, int size) {
-  return UArray2_new(width,height,size);
+    return UArray2_new(width,height,size);
 }
 
 static A2Methods_Array2 new_with_blocksize(int width, int height, int size,
-                                        int blocksize)
+                                           int blocksize)
 {
-  (void) blocksize;
-  return UArray2_new(width,height,size);
+    (void) blocksize;
+    return UArray2_new(width,height,size);
 }
 
- //... many more private (static) definitions follow ...
- static void a2afree (A2A *array) {
-     UArray2_free((UArray2_T *)array);
+//... many more private (static) definitions follow ...
+static void a2afree (A2A *array) {
+    UArray2_free((UArray2_T *)array);
 }
 
-static int width    (A2 array2) { return UArray2_width(array2); }
-static int height   (A2 array2) { return UArray2_height(array2); }
-static int size     (A2 array2) { return UArray2_size(array2); }
-static int blocksize(A2 array2) { assert(array2); return 1; }
+static int width    (A2A array2) { return UArray2_Width(array2); }
+static int height   (A2A array2) { return UArray2_Height(array2); }
+static int size     (A2A array2) { return UArray2_size(array2); }
+static int blocksize(A2A array2) { assert(array2); return 0; }
 
 static A2Methods_Object *at(A2A array, int i, int j) {
     return UArray2_at(array, i, j);
 }
+
+typedef void applyfun(int i, int j, UArray2_T array2, int *elem, void *cl);
+
 static void map_row_major(A2A array2, A2Methods_applyfun apply, void *cl)
 {
+    (void)array2;
     UArray2_map_row_major(array2, (applyfun *) apply, cl);
 }
 
 static void map_col_major(A2A array2, A2Methods_applyfun apply, void *cl)
 {
-    UArray2_map_col_major(array2, (applyfun *) apply, cl);
+    (void)array2;
+    UArray2_map_col_major(array2, (applyfun*) apply, cl);
 }
 
-// now create the private struct containing pointers to the functions
-
 static struct A2Methods_T array2_methods_plain_struct = {
-  new,
-  new_with_blocksize,
-   //... other functions follow in order, with NULL for those not implemented ...
-  a2afree,
-  width,
-  height,
-  size,
-  blocksize,             // blocksize
-  at,
-  map_row_major,
-  map_col_major,
-  NULL,             // map_block_major
-  map_row_major,      // map_default
-  small_map_row_major,
-  small_map_col_major,
-  NULL,             // small_map_block_major
-  small_map_row_major,// small_map_default
+        new,
+        new_with_blocksize,
+        a2afree,
+        width,
+        height,
+        size,
+        blocksize,             // blocksize
+        at,
+        map_row_major,
+        map_col_major,
+        NULL,             // map_block_major
+        map_row_major,      // map_default
 };
 
-// finally the payoff: here is the exported pointer to the struct
 
 A2Methods_T array2_methods_plain = &array2_methods_plain_struct;
+
